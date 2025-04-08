1e2ad948693035bf7239b1e663c7dc50c8ad83dd
acoldwell <abbycoldwell@gmail.com>
2025-04-07 20:18:15 -0500

    4/7/25 plotting/ordination attempt

--- abby.R
+++ abby.R
@@ -1,6 +1,20 @@
-#test file
+library(tidyverse)
+library(vegan)
 
-numbers <- (1:5)
-letters <- c("A","B","C")
+dat <- read.csv("/Users/acoldw1/Documents/Grad/EcologicalAnalysis/data/dat.csv") 
 
-# fixing?
+# want to drop species that have 0 observations
+# only include species data
+dat_spe <- dat %>%
+  subset(select = Acer_negundo:Unknown)
+
+#non_zero <- dat_spe %>%
+
+
+spe_trimmed <- dat_spe %>%
+  select(where(~ sum(.) >= 100)) # refine this cause this filtering method is crazy
+
+
+nmds<-metaMDS(spe_trimmed, distance="bray")
+
+plot(nmds, type = "t")
