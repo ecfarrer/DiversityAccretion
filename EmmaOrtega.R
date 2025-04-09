b9c1e3d4530c790b09e76708374a5b5b2b069aa6
eortegaa <eortega@tulane.edu>
2025-04-09 14:14:56 -0500

    Wendesday April 9 class

--- EmmaOrtega.R
+++ EmmaOrtega.R
@@ -19,8 +19,28 @@ plot(data$communitytype, data$Richness,main="Community Type vs Richness", xlab="
 #Accretion
 plot(data$communitytype, data$Accretion,main="Community Type vs Accretion", xlab="Community Type", ylab="Accretion", pch = 19, col = "#FC9272")
 
-ggplot(data,aes(x=Accretion,y=Richness,color=communitytype))+ 
+ggplot(data,aes(x=Accretion,y=Richness,color=CommunityStationFront))+ 
         geom_point(position = "jitter")+
         labs(x = "Accretion",y="Richness")+ 
         geom_line(stat="smooth",method = "lm",size=.9)+ 
         facet_wrap(~communitytype)
+
+# Total/ Summed cover on accretion
+ggplot(data,aes(x=Accretion,y=SummedCover,color=CommunityStationFront))+ 
+        geom_point(position = "jitter")+
+        labs(x = "Accretion",y="Summed Cover")+ 
+        geom_line(stat="smooth",method = "lm",size=.9, col = "black")+ 
+        ggtitle("Summed Cover on Accretion") + 
+        facet_wrap(~communitytype)
+
+ggplot(data,aes(x=Accretion,y=TotalCover,color=CommunityStationFront))+ 
+        geom_point(position = "jitter")+
+        labs(x = "Accretion",y="Total Cover")+ 
+        geom_line(stat="smooth",method = "lm",size=.9, col = "black")+ 
+        ggtitle("Total Cover on Accretion") + 
+        facet_wrap(~communitytype)
+
+colSums(Filter(is.numeric, data))
+data %>%
+        select_if(is.numeric) %>%
+        map_dbl(sum)
