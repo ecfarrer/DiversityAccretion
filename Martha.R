9555a292faf4cc094b7a6b24af9a7d40f0da027f
Martha Barta <martha239@Marthas-MacBook-Air-2.local>
2025-04-14 14:59:55 -0500

    added graphs and glm

--- Martha.R
+++ Martha.R
@@ -1,20 +1,42 @@
 library(tidyverse)
-library(vegan)
+library(nlme)
 
 data <- read_csv("dat.csv")
 
-#removing species with ≤2 non-zero values
-non_zero_counts <- colSums(data != 0)
-filtered_data <- data[, non_zero_counts >= 3]
+brackish <- data %>%
+  filter(CommunityStationFront == "Brackish")
+
+#dominant species - Spartina patens
+dom_spp <- brackish %>%
+  summarise(across(c("Acer_negundo":"Zizip_Mill."), sum, na.rm = TRUE)) %>%
+  pivot_longer(cols = c("Acer_negundo":"Zizip_Mill."),
+               names_to = "species",
+               values_to = "cover")
 
 
-#removing non-species data
-species <- filtered_data %>%
-  subset(select = Acer_rubrum:Unknown)
+#accretion vs. richness
+ggplot(brackish, aes(x = Richness, y = Accretion)) +
+  geom_point() +
+  geom_smooth(method = lm, se = F)
 
-nmds_result <- metaMDS(species, distance = "bray", k = 2, trymax = 100)
-nmds_result$stress
+#accretion vs. dominant species
+ggplot(brackish, aes(x = Spart_patens, y = Accretion)) +
+  geom_point() +
+  geom_smooth(method = lm, se = F)
+
+#accretion vs. summed cover
+ggplot(brackish, aes(x = SummedCover, y = Accretion)) +
+  geom_point() +
+  geom_smooth(method = lm, se = F)
 
 
-plot(nmds_result, type = "t")
+#glm 
+m1 <- gls(Accretion ~ Richness + Spart_patens + SummedCover, data = brackish)
 
+m2 <- gls(Accretion ~ Richness + Spart_patens + SummedCover, 
+          correlation = corSpher(form = ~ lat+lon), data = brackish)
+  
+anova(m1, m2)
+
+anova(m2, type = "marginal")
+anova(m1, type = "marginal")
